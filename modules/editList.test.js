c3ee9cbb5f7f7a235d7d48aec70237b06d20579a
majdalkilany <majdkilany92@gmail.com>
2023-06-24 18:15:42 +0300

    add edit list function test

---
+++ modules/editList.test.js
@@ -0,0 +1,14 @@
+import editList from './editList.js';
+
+describe('Todo List', () => {
+  test('Edit item function testing', () => {
+    const listArr = [
+      { value: 'Login page design task', completed: false, index: 0 },
+      { value: 'Login page1 design task', completed: false, index: 1 },
+    ];
+    expect(editList(0, 'Login page0', listArr)).toEqual([
+        { value: 'Login page0', completed: false, index: 0 },
+        { value: 'Login page1 design task', completed: false, index: 1 }
+    ]);
+  });
+});
